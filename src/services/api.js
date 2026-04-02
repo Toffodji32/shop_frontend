74da24f93e558ece8904da7ec6854bd32cf8f6fd
Toffodji32 <cedricatchade64@gmail.com>
2026-04-02 09:46:48 +0100

    feat: configure API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { useAuthStore } from '../stores/auth';
 import router from '../router';
 
 const api = axios.create({
-  baseURL: 'https://127.0.0.1:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
